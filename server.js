b04d29e7df60822ec4becc0c7d1b81da084031d6
zcvrdm77fr-dotcom <zcvrdm77fr@privaterelay.appleid.com>
2026-08-26 21:25:28 +0300

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', origin);
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
     res.setHeader('Access-Control-Max-Age', '86400');
   }
   // Vary kertoo välimuisteille, ettei yhden alkuperän vastausta saa tarjoilla toiselle.
