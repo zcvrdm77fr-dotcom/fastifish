fba42566054c54aff7effd6c84fbff5171396c57
zcvrdm77fr-dotcom <zcvrdm77fr@privaterelay.appleid.com>
2026-08-29 03:03:51 +0300

    Add VELMU layer to direct map analysis loading

--- scripts/inject-map-analysis-tags.mjs
+++ scripts/inject-map-analysis-tags.mjs
@@ -5,15 +5,22 @@ import { readFile, writeFile } from 'node:fs/promises';
 const path = process.argv[2] || 'index.html';
 let html = await readFile(path, 'utf8');
 
-const files = ['fishing-structures.js', 'depth-structures.js', 'gtk-substrate.js', 'gtk-habitats.js'];
+const version = 9;
+const files = [
+  'fishing-structures.js',
+  'depth-structures.js',
+  'gtk-substrate.js',
+  'gtk-habitats.js',
+  'velmu-fish.js'
+];
 for (const file of files) {
   const escaped = file.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
   html = html.replace(new RegExp(`\\s*<script\\s+src=["']\\/?${escaped}(?:\\?[^"']*)?["']><\\/script>`, 'gi'), '');
 }
 
-const tags = files.map(file => `<script src="/${file}?v=8"></script>`).join('\n');
+const tags = files.map(file => `<script src="/${file}?v=${version}"></script>`).join('\n');
 if (!html.includes('</body>')) throw new Error('index.html has no </body>');
 html = html.replace('</body>', `${tags}\n</body>`);
 
 await writeFile(path, html, 'utf8');
-console.log(`Injected ${files.length} fishing-map analysis scripts into ${path}`);
+console.log(`Injected ${files.length} fishing-map analysis scripts (v${version}) into ${path}`);
