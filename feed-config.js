297246f1a833af91e5b3d0f2e924d24f4c271e07
zcvrdm77fr-dotcom <zcvrdm77fr@privaterelay.appleid.com>
2026-09-03 17:47:09 +0300

    Lataa pistekalibrointi ja selkeä navigaatio

--- feed-config.js
+++ feed-config.js
@@ -3,17 +3,28 @@ window.FASTFISH_API_BASE = "https://api.fastfishin.com";
 
 // Uudet ominaisuudet pidetään omissa tiedostoissaan, jotta suuri index.html ei kasva entisestään.
 (function loadFastFishingNext(){
-  var css = document.createElement('link');
-  css.rel = 'stylesheet';
-  css.href = '/next-features.css';
-  document.head.appendChild(css);
+  ['/next-features.css', '/site-cleanup.css'].forEach(function(href){
+    var css = document.createElement('link');
+    css.rel = 'stylesheet';
+    css.href = href;
+    document.head.appendChild(css);
+  });
 
   window.addEventListener('load', function(){
-    if (document.querySelector('script[data-fastfishing-next]')) return;
-    var script = document.createElement('script');
-    script.type = 'module';
-    script.src = '/next-features.js';
-    script.dataset.fastfishingNext = '1';
-    document.body.appendChild(script);
+    if (!document.querySelector('script[data-fastfishing-cleanup]')) {
+      var cleanup = document.createElement('script');
+      cleanup.type = 'module';
+      cleanup.src = '/site-cleanup.js';
+      cleanup.dataset.fastfishingCleanup = '1';
+      document.body.appendChild(cleanup);
+    }
+
+    if (!document.querySelector('script[data-fastfishing-next]')) {
+      var script = document.createElement('script');
+      script.type = 'module';
+      script.src = '/next-features.js';
+      script.dataset.fastfishingNext = '1';
+      document.body.appendChild(script);
+    }
   }, { once: true });
 })();
